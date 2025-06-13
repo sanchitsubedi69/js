d9a78df3df2d5cbeff0e0ddf0dfab938c9fed8f7
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-06-13 11:56:22 +0000

    conversion of data types

---
+++ 03_conversionoperations.js
@@ -0,0 +1,65 @@
+// let score=33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let inString= String(score);
+
+// console.log(typeof inString);
+
+// //here we can see that 33 is number and it is converted into string using String() function and saved in the varilabel instring
+//  let maths="66"
+
+//  console.log(typeof maths);
+
+//  let inNumber= Number(maths);
+//   console.log(typeof inNumber);
+
+// console.log("sanchit");
+// let free="33abc"
+
+// console.log(typeof free);
+// let intoNum=Number(free)
+
+// console.log(intoNum);
+
+
+// let free2=null
+// console.log(typeof free2);
+// let intoN=Number(free2)
+// console.log(intoN);
+
+// let free3= undefined
+
+// console.log(typeof free3)
+// let valueinno=String(free3)
+// let valueinn2=String(free3)
+// console.log(typeof valueinno)
+
+// "33"=>33
+//"33abc"=>NaN
+//true=> 1;
+// false =>0;
+
+// let isLoggedIn= 1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// // console.log(typeof booleanIsLoggedIn);
+// let isLoggedIn= ""
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn);
+
+//1=>true; 0=> false
+//""=> false
+// "hitesh"=> true
+
+let someNumber = 33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
