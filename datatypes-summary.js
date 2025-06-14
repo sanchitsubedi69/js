505532d6e2915205d26c510d815b7db7cd33d40f
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-06-14 04:54:31 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,43 @@
+//primitive
+
+// 7 types: string,Number, Boolean,Null,undefined,symbol,BigInt
+
+// const score= 100
+// const scorevalue= 100.3
+
+// const isLoggedIn = false
+const outsideTemp = null
+// let userEmail;
+ 
+// // const id = symbol('123')
+// // const anotherId = symbol('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 1273648274974528076n
+// console(typeof bigNumber)
+
+
+// //Reference (NOn primitive)
+// //Array,objects, Functions
+
+// const heros= ["shaktiman","naagraj","doga"]
+// let myObj={// key value pairs ma hunxa object
+//     name: "sanchit",
+//     age: 22,
+
+
+
+
+
+const myFunction = function(){
+    console.log("Hello World");
+
+    
+
+}
+myFunction();
+
+
+console.log(typeof outsideTemp ) 
+
