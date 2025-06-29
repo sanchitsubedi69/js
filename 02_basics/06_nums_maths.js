d35fdac8e740c919429c7f9e203e093054086724
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-06-29 02:33:20 +0000

    math and num

---
+++ 02_basics/06_nums_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.66
+
+// console.log(otherNumber.toPrecision(3))
+
+const hundered = 1000000
+// console.log(hundered.toLocaleString('en-IN'));
+
+
+// =================maths_++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.44))
+// console.log(Math.min(4,3,5,7));
+// console.log(Math.max(4,3,5,7));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1) + min));
+
+
+
+
+
+
+
+
