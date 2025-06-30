8b1fd442a5ac3d7e9ac9fb286ac8e5e349d7b3a3
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-06-30 11:06:54 +0000

    date done basics

--- 02_basics/07_date.js
+++ 02_basics/07_date.js
@@ -16,9 +16,22 @@ let createdMyDate= new Date("01-23-2033")
 
 let myTimeStamp = Date.now()
 
-console.log(myTimeStamp)
-console.log(createdMyDate.getTime())
-console.log(createdMyDate.getTime())
+// console.log(myTimeStamp)
+// console.log(createdMyDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+// console.log(`${newDate.toLocaleString()}  and the time is ${newDate.getTime()}`)
+
+console.log(newDate.toLocaleString('default',{weekday:"long"}))
+
+
+
+
 
 
 
