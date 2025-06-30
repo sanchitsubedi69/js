43b60c2806e8d3e2905fbe58ecbe077d0a6e6a64
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-06-30 10:53:09 +0000

    date done

---
+++ 02_basics/07_date.js
@@ -0,0 +1,27 @@
+// let  myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let createdMyDate= new Date(2023,0,23)
+// console.log(createdMyDate.toDateString());
+// console.log(createdMyDate.toLocaleString());
+// console.log(createdMyDate.toString());
+
+// let createdMyDate= new Date("2023-01-23")
+let createdMyDate= new Date("01-23-2033")
+// console.log(createdMyDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp)
+console.log(createdMyDate.getTime())
+console.log(createdMyDate.getTime())
+
+
+
+        
+    
+    
