5c7a3bf1b7193c6073796774d0f1c85a9db6ef8a
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-08-19 17:30:25 +0000

    obj

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -1,53 +1,70 @@
 // const tinderUser = new Object()
-const tinderUser ={}
+// const tinderUser ={}
 
-tinderUser.id="123abc"
-tinderUser.name="samy"
-tinderUser.isLoggedIn = false
+// tinderUser.id="123abc"
+// tinderUser.name="samy"
+// tinderUser.isLoggedIn = false
 
+// // console.log(tinderUser);
+
+// const regularUser ={
+//     email: "sanchit@gmail.com",
+//     fullname: {
+//         userfullname:{
+//             firstname:"sanchit",
+//             lastname:"subedi"
+//         }
+//     }
+// }
+// // console.log(regularUser.fullname.userfullname.firstname);
+
+// const obj1={
+//     1:"a",2:"b"
+// }
+
+// const obj2={
+//     3:"a",4:"b"
+// }
+
+// // const obj3={obj1,obj2}
+// // const obj3= Object.assign({},obj1,obj2)
+// const obj3={...obj1,...obj2}
+// // console.log(obj3);
+
+// const user =[
+//     {
+//         id:1,
+//         email:"sanchit@gmail.com"
+//     },
+//     {
+
+//     },
+//     {},
+
+
+// ]
+// user[1].email;
 // console.log(tinderUser);
 
-const regularUser ={
-    email: "sanchit@gmail.com",
-    fullname: {
-        userfullname:{
-            firstname:"sanchit",
-            lastname:"subedi"
-        }
-    }
-}
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(Object.keys(tinderUser));  //keys nikalney syntax
+// console.log(Object.values(tinderUser));  //values nikalney syntax
+// console.log(Object.entries(tinderUser)); //keys ans values array ko form ma nikal dinxa
 
-const obj1={
-    1:"a",2:"b"
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));// to check the given thing is inside the object or not
+
+
+const course={
+    coursename:"js in Nepali",
+    price:"599",
+    courseInstructor:"kingkohli"
 }
 
-const obj2={
-    3:"a",4:"b"
-}
+// another syntax to extracting the value
 
-// const obj3={obj1,obj2}
-// const obj3= Object.assign({},obj1,obj2)
-const obj3={...obj1,...obj2}
-// console.log(obj3);
+const {price: pp}=course
 
-const user =[
-    {
-        id:1,
-        email:"sanchit@gmail.com"
-    },
-    {
-
-    },
-    {},
+console.log(pp);
 
 
-]
-user[1].email;
-console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));  //keys nikalney syntax
-console.log(Object.values(tinderUser));  //values nikalney syntax
-console.log(Object.entries(tinderUser)); //keys ans values array ko form ma nikal dinxa
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));// to check the given thing is inside the object or not
