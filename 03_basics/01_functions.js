5297142102189dd73bc7d1ab54df2ca1ebfdff08
Sanchit Subedi <161282418+sanchitsubedi69@users.noreply.github.com>
2025-09-22 02:54:29 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,35 +1,70 @@
-function sayMyName(){
-    console.log("S");
-console.log("A");
-console.log("N");
-console.log("C");
-console.log("H");
-}
+// function sayMyName(){
+//     console.log("S");
+// console.log("A");
+// console.log("N");
+// console.log("C");
+// console.log("H");
+// }
 
-// sayMyName()
+// // sayMyName()
 
-function addTwoNumbers(number1, Number2){
+// function addTwoNumbers(number1, Number2){
 
     
-    let result= number1+ Number2
-    return result
-    console.log(number1+Number2);// here this console doesnot work because there is return above it
+//     let result= number1+ Number2
+//     return result
+//     console.log(number1+Number2);// here this console doesnot work because there is return above it
     
+// }
+// const result= addTwoNumbers(3,null);
+
+// // console.log(result);
+
+// // conclusion is that if we want to store the result in the variable then we must return at first
+
+// function loginUserMessage(username) {
+//     if(!username){
+//         console.log("please enter a username")
+//         return
+
+//     }
+//     return `${username} just logged in`
+    
+// }
+// // console.log(loginUserMessage("sanchit"))
+// console.log(loginUserMessage())
+
+
+function calculateCartPrice(val1,val2,...num1){// this is rest or  dot operator ...num1
+    return num1
 }
-const result= addTwoNumbers(3,null);
 
-// console.log(result);
 
-// conclusion is that if we want to store the result in the variable then we must return at first
+// console.log(calculateCartPrice(200,300,400,5000,3000))
 
-function loginUserMessage(username) {
-    if(!username){
-        console.log("please enter a username")
-        return
+const user ={
+    username:"sanchit",
+    price:200
+}
 
+function handleObject(anyobject){
+console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username:"san",
+        price:69
     }
-    return `${username} just logged in`
-    
+)
+
+const myNewArray =[200,300,400,4500]
+
+function returnSecondValue(getArray){
+    return getArray[3]
 }
-// console.log(loginUserMessage("sanchit"))
-console.log(loginUserMessage())
+// console.log(returnSecondValue(myNewArray));
+ console.log(returnSecondValue([200,300,400,4500]));
+
+
